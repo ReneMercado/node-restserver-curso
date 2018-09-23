4372b5e011751a46c7f582b99267b936e92ef566
Rene Mercado <rene.m@financieramas.app>
2018-09-23 10:51:35 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,23 @@
 // ===========================
 //EN HEROKU SE ACTUALIZA AUTOMATICAMENTE
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===========================
+// ENTORNO
+// ===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; //heroku pone la variable NODE_ENV pero en otros servicios hacen algo igual 
+
+
+// ===========================
+// ENTORNO
+// ===========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:123456_R@ds111993.mlab.com:11993/cafe';
+}
+
+process.env.URLDB = urlDB;
